b9fa290dab69243d953c5970ceabab3597b55c44
Joona Kääriäinen <kaariainenjoona@gmail.com>
2026-01-29 22:38:33 +0200

    unicafe steps 3 and 4

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -28,15 +28,27 @@ const App = () => {
         <button onClick={increasebad}>
         bad
       </button>
-      <h1>{parts.headers[2]}</h1>  
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good * 1 + neutral * 0 + bad * (-1))/(good+neutral+bad)}</p>
-      <p>positive {good / (good+neutral+bad)} %</p>
+      <h1>{parts.headers[2]}</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>  
     </div>
   )
 }
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  
+  if (all==0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {(props.good * 1 + props.neutral * 0 + props.bad * (-1))/(all)}</p>
+      <p>positive {props.good / (all)} %</p>
+    </div>
+  )
+}
 export default App
